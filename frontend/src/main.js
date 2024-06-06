d4f6cadc5845177ec95f31a507dfad289ea6b5d9
Alexa N <alexa.nc.kitsune@gmail.com>
2024-06-05 20:53:47 -0600

    changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
-app.config.globalProperties.$ENDPOINT = 'http://192.168.100.5:5000' // Replace with backend server.
+app.config.globalProperties.$ENDPOINT = 'http://localhost:5000' // Replace with backend server.
 
 app.mount('#app')
